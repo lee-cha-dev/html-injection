20cc099182e7ed4239ffaa6e209a0920275132df
lee-cha-dev <leecharlesdev@gmail.com>
2023-05-10 04:50:53 -0500

    updated paths

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,18 @@
 
-
-const Navbar = () => {
-    const dev = false;
-    if (dev === true){
-        return (
-            "<nav class='navbar'>" +
-            "   <a href='index.html'>LC</a>" +
-            "   <a href='portfolio.html'>Portfolio</a>" +
-            "</nav>"
-        )
-    }
-    return (
-        "<nav class='navbar'>" +
+const dev = false;
+let nav;
+if (dev === true){
+    nav = "<nav class='navbar'>" +
+        "   <a href='index.html'>LC</a>" +
+        "   <a href='portfolio.html'>Portfolio</a>" +
+        "</nav>";
+} else {
+    nav = "<nav class='navbar'>" +
         "   <a href='./'>LC</a>" +
         "   <a href='/html-injection/portfolio'>Portfolio</a>" +
-        "</nav>"
-    )
+        "</nav>";
+}
+
+const Navbar = () => {
+    return nav;
 }
